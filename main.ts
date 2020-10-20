f7fef564e66e9627cc854427243e3b0842888798
fu1819 <71689568+fu1819@users.noreply.github.com>
2020-10-20 15:45:22 +0800

    變斜線了

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
-let Y = 0
 let X = 0
+let Y = 0
 basic.forever(function () {
-    Y = 4
-    while (Y <= 4) {
-        if (Y / 2 == 0) {
-            while (true) {
+    X = 4
+    while (X <= 4) {
+        if (X % 2 == 0) {
+            Y = 4
+            while (Y <= 4) {
                 led.plot(X, Y)
                 basic.pause(500)
                 Y += -1
