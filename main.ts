17c15916a00693bf545821b0e290e768fd46d1a2
fu1819 <71689568+fu1819@users.noreply.github.com>
2020-10-26 23:38:21 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,15 +2,15 @@ let X = 0
 let Y = 0
 basic.forever(function () {
     X = 4
-    while (X <= 4) {
-        if (X % 2 == 0) {
-            Y = 4
-            while (Y <= 4) {
-                led.plot(X, Y)
-                basic.pause(500)
-                Y += -1
-                X += -1
-            }
+    Y = 0
+    while (X <= 4 && Y <= 4) {
+        led.plot(X, Y)
+        if (X == 0 && Y == 4) {
+            led.plot(X, Y)
+        } else if (false) {
+        	
+        } else {
+        	
         }
     }
 })
